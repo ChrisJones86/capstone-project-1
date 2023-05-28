92fb20d21fb4e5cfb5ef3760d603b27c635b7d06
ChrisJones86 <110697929+ChrisJones86@users.noreply.github.com>
2023-05-28 11:57:27 -0700

    finishing up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,76 @@
-import './App.css';
-import Header from './Components/Header.js';
-import Nav from './Components/Nav.js';
-import Main from './Components/Main.js';
-import Footer from './Components/Footer.js';
+import "./App.css";
+import Home from "./components/Home";
+import BookingPage from "./components/BookingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useReducer, useState } from "react";
+import { fetchAPI, submitAPI } from "./API/Api";
+import ConfirmedBooking from "./components/ConfirmedBooking";
 
 function App() {
-  return (
-    <>
-      <Header/>
-      <Nav/>
-      <Main/>
-      <Footer/>
+  const [bookingData, setBookingData] = useState({
+    date: " ",
+    time: " ",
+    guests: " ",
+    occasion: " ",
+  });
 
-    </>
+  
+  const today = new Date();
+
+  const init = { times: fetchAPI(today) };
+
+  const [availableTimes, setAvailableTimes] = useReducer(updateTimes, init);
+
+  const submitForm = () => {
+    const res = submitAPI(bookingData);
+    if (res === true) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  function initializeTimes() {
+    setAvailableTimes({ type: "init" });
+  }
+
+  function updateTimes(state, action) {
+    if (action.type === "init") {
+      return { times: fetchAPI(today) };
+    }
+    if (action.type === "update_times") {
+      let selectedBookingDate = new Date(bookingData.date);
+      let newTimes = fetchAPI(selectedBookingDate).filter(
+        (time) => time !== "17:00"
+      );
+      return { times: newTimes };
+    }
+    throw Error("Unknown action.");
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route index element={<Home bookingData={bookingData} />} />
+        <Route
+          path="reservations"
+          element={
+            <BookingPage
+              bookingData={bookingData}
+              setBookingData={setBookingData}
+              availableTimes={availableTimes}
+              setAvailableTimes={setAvailableTimes}
+              submitForm={submitForm}
+            />
+          }
+        />
+
+        <Route
+          path="confirmation"
+          element={<ConfirmedBooking bookingData={bookingData} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
