f9a7fcba75446c8ef1c8a5db082330e2237ef6d9
ChrisJones86 <110697929+ChrisJones86@users.noreply.github.com>
2023-05-28 11:45:34 -0700

    Finished Main content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import Header from './Components/Header.js';
+import Nav from './Components/Nav.js';
+import Main from './Components/Main.js';
+import Footer from './Components/Footer.js';
 
 function App() {
   return (
-    <div className="">
+    <>
       <Header/>
       <Nav/>
       <Main/>
       <Footer/>
 
-    </div>
+    </>
   );
 }
 
