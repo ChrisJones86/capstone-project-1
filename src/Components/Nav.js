2675a2b5d8c1cac73c3fb1399bbc4e6c62af4c9f
ChrisJones86 <110697929+ChrisJones86@users.noreply.github.com>
2023-06-01 13:07:35 -0700

    Trimmed the fat

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,43 +1,20 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import littlelemon_logo from "../images/littlelemon_logo.png"
 
-function Nav() {
-  return (
-    <>
-      <ul>
-        <li>
-          <Link to="/" className="a">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="a">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="a">
-            Menu
-          </Link>
-        </li>
-        <li>
-          <Link to="/reservations" className="a">
-            Reservations
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="a">
-            Order Online
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="a">
-            Login
-          </Link>
-        </li>
-      </ul>
-    </>
-  );
+const Nav = () => {
+    return(
+        <nav>
+            <img src={littlelemon_logo} alt="Little Lemon Logo" ></img>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Menu</a></li>
+                <li><a>Reservations</a></li>
+                <li><a>Order Online</a></li>
+                <li><a>Login</a></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Nav;
