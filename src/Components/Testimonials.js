57bb2defe28f4d8da74c207dcb255056fe00d788
ChrisJones86 <110697929+ChrisJones86@users.noreply.github.com>
2023-06-02 15:38:46 -0700

    Added Testimonials and About pages

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Testimonials = () => {
+    return(
+        <mid>
+            <section>
+                <div>Testimonials</div>
+            </section>
+            <section>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+            </section>
+        </mid>
+    )
+}
+
+export default Testimonials;
